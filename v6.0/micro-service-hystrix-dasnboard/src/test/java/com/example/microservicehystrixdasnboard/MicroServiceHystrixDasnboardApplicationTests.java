1265b1d0f5d2aef59bd045bb2a3dc7bf6489d62c
gengyuzhong <gengyuzhong@zzpxx.com>
2019-06-20 19:13:52 +0800

    增加zuul网关代理，实现统一代理eureka上注册的服务

---
+++ v6.0/micro-service-hystrix-dasnboard/src/test/java/com/example/microservicehystrixdasnboard/MicroServiceHystrixDasnboardApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.microservicehystrixdasnboard;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MicroServiceHystrixDasnboardApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
---
+++ v6.0/readme.md
@@ -0,0 +1,5 @@
+## v6.0
+1.添加zuul网关 由网关来代理eureka上注册的所有微服务 （反向代理）
+2.路由规则如下  serviceId/api  （需要小写）
+3.zuul本身会注册到eureka 上
+
